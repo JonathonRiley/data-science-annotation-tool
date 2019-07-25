46b1a06c61eaabc5fa21eb5a54289769f85ad5fa
Jonathon Riley <jonathonriley1990@gmail.com>
2019-07-25 22:00:27 +0100

    WiP - sorting highlighting and page loading

--- static/js/ner_annotator.js
+++ static/js/ner_annotator.js
@@ -1,4 +1,8 @@
 function initialise(id){
+    var url = document.URL;
+    var splitUrl = url.split('/');
+    sessionStorage.setItem('id', Number(splitUrl[5]));
+    sessionStorage.setItem("textIndex", 0);
     var labelsRaw = JSON.parse(sessionStorage.getItem("globalLabels"));
     var labelsString ='';
     var i;
@@ -11,13 +15,19 @@ function initialise(id){
     var content = textEntities[Number(id)]["content"].toString();
     document.getElementById("text").innerHTML = content;
 
-    for (var entity in textEntities[Number(id)]["entities"]){
-        var textStart = entity[0];
-        var textEnd = entity[1];
-        var colour = labelsRaw[entity[2]]["colour"];
-        addEntityHTML("cat",colour);
+    for (var ii =0; ii < textEntities[Number(id)]["entities"].length; ii++){
+        var childArray = textEntities[Number(id)]["entities"][ii];
+        var start = childArray[0];
+        var end = childArray[1];
+        var colour = labelsRaw[childArray[2]]["colour"];
+        var range = document.createRange();
+        range.setStart(startNode, start);
+        range.setEnd(endNode, end);
+
+        highlightRange(content.substring(start,end), colour);
+        addEntityHTML(content.substring(start,end),colour);
     }
-    document.getElementById("test").innerHTML = sessionStorage.getItem("textEntities");
+
 }
 
 function prevPage(){
@@ -39,18 +49,27 @@ function newEntity(labelsString, label, colour, labelCount) {
 }
 
 function addToEntity(label, colour){
-    var selection = window.getSelection();
+    var selection = window.getSelection().getRangeAt(0);
     var labelsRaw = JSON.parse(sessionStorage.getItem("globalLabels"));
-    var id = sessionStorage.getItem("id");
+    var id = document.URL.split('/')[5];
+    var textIndex = sessionStorage.getItem("textIndex");
     var entities = JSON.parse(sessionStorage.getItem("textEntities"));
+    var content = entities[Number(id)]["content"];
+    var start = content.indexOf(selection,textIndex);
+    if (start = -1){
+        start = content.indexOf(selection,0)
+    };
+    var end = start + selection.toString().length;
+    sessionStorage.setItem("textIndex", end);
+    entities[Number(id)]["entities"].push([start, end, label]);
 
-    highlightRange(selection.getRangeAt(0), colour);
+    highlightRange(selection, colour);
     addEntityHTML(selection, colour);
     labelsRaw[label]["counts"] += 1;
     sessionStorage.setItem("globalLabels", JSON.stringify(labelsRaw));
-    entities[Number(id)]["entities"].push([selection.getRangeAt(0).startOffset, selection.getRangeAt(0).endOffset, label]);
-    document.getElementById("test2").innerHTML = entities[Number(id)]["entities"];
     sessionStorage.setItem("textEntities", JSON.stringify(entities));
+
+
 }
 
 function addEntityHTML(word, colour){
