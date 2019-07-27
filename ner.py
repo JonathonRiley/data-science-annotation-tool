c63827c6c9c1af3310a1c64e8fb0985b2574f773
Jonathon Riley <jonathonriley1990@gmail.com>
2019-07-27 20:53:24 +0100

    remove unused ner.py file

--- ner.py
+++
@@ -1,8 +0,0 @@
-from startup import app
-from flask import Flask, redirect, url_for, request
-from flask import render_template
-
-@app.route('/ner/home', methods=['GET', 'POST'])
-def ner_home():
-
-    return render_template('labels.html')
